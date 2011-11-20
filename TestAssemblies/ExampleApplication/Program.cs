031b8f9677c0cdbc22be0eb664866d64dc6caa78
rgray@rgray7.engdev.arcww.com <rgray@rgray7.engdev.arcww.com>
2011-11-20 01:47:06 -0600

    Instrumentation won't work unless the Registry is populated at the time of execution. I have one idea on how to do this: make a (static) local registry in the executing assembly. On second thought, it might be best to create a separate assembly just for the purpose of housing the registry. Code under test could reference this assembly as well.

---
+++ TestAssemblies/ExampleApplication/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace ExampleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length != 1) throw new ArgumentException("A name must be supplied.");
+
+            var name = args[0];
+            var newModel = new Model();
+            newModel.Name = name;
+
+            var insert = new InsertCommand();
+            insert.Arg = newModel;
+            insert.Execute();
+
+            Console.WriteLine("Press any key to exit..");
+            Console.ReadKey();
+        }
+    }
+
+    public static class Dao
+    {
+        private static int idSeed = 0;
+
+        public static int Insert(Model newModel)
+        {
+            Console.WriteLine("Inserting '{0}'", newModel.Name);
+            return ++idSeed;
+        }
+    }
+
+    public class InsertCommand
+    {
+        public Model Arg { get; set; }
+
+        public void Execute()
+        {
+            Dao.Insert(Arg);
+        }
+    }
+
+    public class Model
+    {
+        internal string Name { get; set; }
+    }
+}
---
+++ Tests/ExampleUsages/Examples.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+using ExampleApplication;
+using NUnit.Framework;
+using SharpMock.Core.Syntax;
+
+namespace ExampleUsages
+{
+    [TestFixture]
+    public class Examples
+    {
+        [Test]
+        public void CanInstrumentAssembly()
+        {
+            var intercept = new Faker();
+            intercept.CallsTo(() => Dao.Insert(null)).ByReplacingWith(
+                (Model m) =>
+                    {
+                        Console.WriteLine("Stopping insert.");
+                        return 99;
+                    });
+
+            Program.Main(new string[] { "Hello" });
+
+            var startInfo = new ProcessStartInfo("ExampleApplication.exe", "Boo!");
+            var process = new Process();
+            process.StartInfo = startInfo;
+            process.Start();
+
+            Assert.Fail("Instrumentation ability has not been implemented yet.");
+        }
+    }
+}
