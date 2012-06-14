f84c0c301ffec865dff5fc494c6e84663cbb8cc8
jamarchist <jamesryangray@gmail.com>
2012-06-14 13:34:14 -0500

    Housekeeping
    --HG--
    rename : Tests/IntegrationTests/IntegrationTests/InterfaceTests.cs => Tests/IntegrationTests/IntegrationTests/WhenFakingInterfaces.cs

---
+++ Tests/IntegrationTests/IntegrationTests/WhenFakingInterfaces.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using ScenarioDependencies;
+using Scenarios;
+using SharpMock.Core.Syntax;
+
+namespace IntegrationTests.IntegrationTests
+{
+    [TestFixture]
+    public class WhenFakingInterfaces
+    {
+        [Test]
+        public void MethodCallIsIntercepted()
+        {
+            var wasCalled = false;
+
+            var fake = new Faker();
+            fake.CallsTo<ISomeInterface>(i => i.DoSomething()).ByReplacingWith(() => wasCalled = true);
+
+            var code = new CodeUnderTest();
+            code.CallsSomeInterface(null);
+
+            Assert.IsTrue(wasCalled);
+        }
+    }
+}
