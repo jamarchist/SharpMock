72d38b44680bdb8de20b42847c9ccfeef11b260a
jamarchist <jamesryangray@gmail.com>
2012-06-14 15:58:24 -0500

    Housekeeping
    --HG--
    rename : Tests/IntegrationTests/TypeTranslationTests/DelegatesTests.cs => Tests/IntegrationTests/TypeTranslationTests/WhenTranslatingTypes.cs

---
+++ Tests/IntegrationTests/TypeTranslationTests/WhenTranslatingTypes.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Cci;
+using NUnit.Framework;
+using SharpMock.Core;
+using SharpMock.Core.PostCompiler.Construction.Reflection;
+
+namespace IntegrationTests.TypeTranslationTests
+{
+    [TestFixture]
+    public class WhenTranslatingTypes
+    {
+        [Test]
+        public void GenericDelegatesAreHandled()
+        {
+            var host = new PeReader.DefaultHost();
+            host.LoadUnit(host.CoreAssemblySymbolicIdentity);
+            host.LoadUnitFrom("SharpMock.Core.dll");
+
+            var reflector = new UnitReflector(host);
+            var cciType = reflector.Get<VoidAction<string>>();
+
+            Assert.IsInstanceOf<IGenericTypeInstanceReference>(cciType);
+        }
+
+        [Test]
+        public void OpenGenericTypesAreHandled()
+        {
+            var host = new PeReader.DefaultHost();
+            host.LoadUnit(host.CoreAssemblySymbolicIdentity);
+            host.LoadUnitFrom("SharpMock.Core.dll");
+
+            var reflector = new UnitReflector(host);
+            var cciType = reflector.Get(typeof(VoidAction<>));
+
+            Assert.IsInstanceOf<INamespaceTypeReference>(cciType);
+        }
+    }
+}
